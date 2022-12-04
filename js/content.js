653a4e10b6861afa310c4dde5f6444ede7d68f70
william-cass-wright <william.cass.wright@gmail.com>
2022-12-04 01:39:20 -0800

    first

---
+++ js/content.js
@@ -0,0 +1,34 @@
+console.log('content.js loaded');
+
+function unsecuredCopyToClipboard(text) {
+  console.log(text);
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    console.error('Unable to copy to clipboard', err);
+  }
+  document.body.removeChild(textArea);
+  console.log('unsecured successful');
+}
+
+function copyToClipboard(content) {
+  if (window.isSecureContext && navigator.clipboard) {
+    console.log('navigator.clipboard.writeText');
+    navigator.clipboard.writeText(content);
+  } else {
+    console.log('unsecuredCopyToClipboard');
+    unsecuredCopyToClipboard(content);
+  }
+};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // 2. A page requested user data, respond with a copy of `user`
+  console.log('message recieved');
+  console.log(message.copyText);
+  copyToClipboard(message.copyText);
+});
